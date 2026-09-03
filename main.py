3dc8e27d580bf25bf94fa04afdfcc662c3b02f5b
Nitish Kumar <134670567+Nitishkumar000@users.noreply.github.com>
2026-09-03 12:53:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from admin import Admin
+
+def main():
+    ch = 0
+    while(ch != '2'):
+        print('''---EMPLOYEE MANAGEMENT SYSTEM---
+        1. Login
+        2. Exit''')
+        ch = input('Enter choice:')
+        if(ch == '1'):
+            uname = input('Enter username:')
+            passw = input('Enter password:')
+            if(uname == 'admin' and passw == '1234'):
+                print('Logged in successful')
+                Admin()
+            else:
+                print('Invalid credentials....')
+        elif(ch == '2'):
+            print('Thank you!')
+        else:
+            print('Invalid choice...')
+
+if(__name__ == '__main__'):
+    main()
